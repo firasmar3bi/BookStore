8268c5de6a21cc2a16dfe45daef7b2507661a818
firasmar3bi <mfiras190@gmail.com>
2024-05-18 00:36:24 +0300

    Add project files.

---
+++ BookStore/Controllers/CategorysController.cs
@@ -0,0 +1,19 @@
+﻿using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CategorysController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public CategorysController(ApplicationDbContext context) {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var category = context.Categorys.ToList();
+            return View(category);
+        }
+    }
+}
---
+++ BookStore/Models/Categorys.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Categorys
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string name { get; set; } = null!;
+
+        public DateTime createDate { get; set; } = DateTime.Now;
+
+        public DateTime updateDate { get; set; } = DateTime.Now;
+ 
+    }
+}
